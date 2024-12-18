bd01cc82ddab262758edc78a2bb0b6cd4e9335e5
Jack Jee <166012340+JackJee0812@users.noreply.github.com>
2024-12-18 17:30:35 +0530

    add

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// const triangle = document.querySelector('.triangle');
+
+// window.addEventListener('scroll', () => {
+   
+//     const scrollPosition = window.scrollY;
+
+
+//     const newLeftPosition = 50 - scrollPosition * 0.2; 
+//     const rotationAngle = scrollPosition * 1; 
+
+  
+//     triangle.style.left = `${newLeftPosition}%`;
+//     triangle.style.transform = `translateX(-120%) rotate(${rotationAngle}deg)`;
+// });
+
+
+
+
+// const triangles = document.querySelector('.triangles');
+
+// window.addEventListener('scroll', () => {
+   
+//     const scrollPosition = window.scrollY;
+
+
+//     const newLeftPosition = 50 - scrollPosition * 0.2; 
+//     const rotationAngle = scrollPosition * 1; 
+
+  
+//     triangle.style.left = `${newLeftPosition}%`;
+//     triangle.style.transform = `translateX(-120%) rotate(${rotationAngle}deg)`;
+// });
