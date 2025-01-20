eccb2605d82c52b6144247981603273df67fe664
Jack Jee <166012340+JackJee0812@users.noreply.github.com>
2025-01-20 18:21:35 +0530

    details

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
 
-// const triangle = document.querySelector('.triangle');
-
-// window.addEventListener('scroll', () => {
-   
-//     const scrollPosition = window.scrollY;
-
-
-//     const newLeftPosition = 50 - scrollPosition * 0.2; 
-//     const rotationAngle = scrollPosition * 1; 
-
-  
-//     triangle.style.left = `${newLeftPosition}%`;
-//     triangle.style.transform = `translateX(-120%) rotate(${rotationAngle}deg)`;
-// });
-
-
-
-
-// const triangles = document.querySelector('.triangles');
-
-// window.addEventListener('scroll', () => {
-   
-//     const scrollPosition = window.scrollY;
-
-
-//     const newLeftPosition = 50 - scrollPosition * 0.2; 
-//     const rotationAngle = scrollPosition * 1; 
-
-  
-//     triangle.style.left = `${newLeftPosition}%`;
-//     triangle.style.transform = `translateX(-120%) rotate(${rotationAngle}deg)`;
-// });
-
-
-
-
 
 
     // Add underline animation to all nav links and dropdown items
