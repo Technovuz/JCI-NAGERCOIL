ffe1191f4f2702d8f2513b5dd9fcfb7c123dd842
kanal-kannan <kanalkannan@technovuz.com>
2025-01-24 12:47:29 +0530

    remove api

--- jci API/src/routes/adminroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginAdmin } = require('../controller/admincontroller');
-
-// Login route for admin
-router.post('/login', loginAdmin);
-
-module.exports = router;
